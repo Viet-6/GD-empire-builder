072b3cbfb481cc0e7047403cc140f027e7e14d06
Viet-6 <vlehoangquoc@gmail.com>
2026-02-03 08:40:14 +0000

    Phase 2-1

--- scripts/core/Main.gd
+++ scripts/core/Main.gd
@@ -1,7 +1,13 @@
 extends Control
 
+@onready var trading_ui_scene = preload("res://scenes/TradingUI.tscn")
+
 func _ready():
 	print("Main Scene Ready")
+	
+	var ui_instance = trading_ui_scene.instantiate()
+	add_child(ui_instance)
+	
 	# Simulate a few turns
 	GameManager.advance_turn()
 	await get_tree().create_timer(0.5).timeout
